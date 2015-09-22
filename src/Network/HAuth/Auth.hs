5eb86c07ca0839e3dfa7469e782de4d3480185b3
Tim Dysinger <tim@fpcomplete.com>
2015-09-22 22:19:09 +0000

    reduce authMac signature footprint

--- src/Network/HAuth/Auth.hs
+++ src/Network/HAuth/Auth.hs
@@ -13,16 +13,20 @@ import Network.HAuth.Types
 import Network.HTTP.Types
 import Network.Wai
 
-authMac :: TS -> Nonce -> Maybe Ext -> Request -> Secret -> Mac
-authMac (TS ts) (Nonce nonce) ext rq (Secret key) =
+authMac :: Auth -> Request -> Secret -> Mac
+authMac Auth{ts = (TS t),nonce = (Nonce n),..} rq (Secret key) =
     let attrs =
-            [ (pack . show) ts
-            , nonce
+            [ (pack . show) t
+            , n
             , (requestMethod rq)
             , (rawPathInfo rq)
             , maybe "" id (lookup "host" (requestHeaders rq))
             , (pack . show) 443
-            , maybe "" (\(Ext e) -> e) ext]
+            , maybe
+                  ""
+                  (\(Ext e) ->
+                        e)
+                  ext]
         hmac' :: HMAC SHA256
         hmac' = hmac key ((intercalate "\n" attrs) <> "\n")
     in Mac (toBytes hmac')
