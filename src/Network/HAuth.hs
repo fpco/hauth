c73b2f37ea619b7bc45f36ecd8dddf35f531e82e
Tim Dysinger <tim@fpcomplete.com>
2015-09-28 21:11:11 +0000

    follow the wiki-doc closely on http status codes
    (which failures go with which http status code.)

--- src/Network/HAuth.hs
+++ src/Network/HAuth.hs
@@ -15,8 +15,6 @@
 module Network.HAuth (module Network.HAuth.Types, hauthMiddleware)
        where
 
--- TODO check that I'm returning the right 40x code as per the wiki doc
-
 #if __GLASGOW_HASKELL__ < 710
 import           Control.Applicative ((<$>), (<*))
 #endif
@@ -41,7 +39,8 @@ import           Network.HAuth.Consul
 import           Network.HAuth.Parse
 import           Network.HAuth.Postgres
 import           Network.HAuth.Types
-import           Network.HTTP.Types (status401, hAuthorization)
+import           Network.HTTP.Types
+       (status400, status401, status403, hAuthorization)
 import           Network.Wai (responseLBS, requestHeaders, Middleware)
 import           STMContainers.Map (Map)
 
@@ -55,48 +54,48 @@ hauthMiddleware client cache pool app rq respond =
     checkAuthHeader = do
         reqId <- liftIO nextRandom
         case lookup hAuthorization (requestHeaders rq) of
-            Nothing -> authFailure reqId "missing"
+            Nothing -> authFailure status401 reqId "missing header"
             Just bs ->
                 case parseOnly (authHeaderP <* endOfInput) (T.decodeUtf8 bs) of
-                    Left err -> authFailure reqId (T.pack err)
+                    Left err -> authFailure status401 reqId (T.pack err)
                     Right authHeader ->
                         case authHeaderToAuth authHeader of
-                            Left err -> authFailure reqId (T.pack err)
+                            Left err -> authFailure status400 reqId (T.pack err)
                             Right auth -> do
                                 checkAuthMAC reqId auth
     checkAuthMAC reqId auth@Auth{..} = do
         maybeAcct <- getAccount client cache authID
         case maybeAcct of
-            Nothing -> authFailure reqId "invalid id"
+            Nothing -> authFailure status403 reqId "invalid id"
             Just Account{..} -> do
                 let computedMAC =
                         hmacDigest authTS authNonce authExt rq acctSecret
                 liftIO (print computedMAC)
                 if authMAC /= computedMAC
-                    then authFailure reqId "invalid mac"
+                    then authFailure status403 reqId "invalid mac"
                     else checkAuthTS reqId auth
     checkAuthTS reqId auth@Auth{authTS = AuthTS ts,..} = do
         timeSpread <- abs . (-) ts . floor <$> liftIO getPOSIXTime
         if timeSpread > 120
-            then authFailure reqId "invalid timestamp"
+            then authFailure status403 reqId "invalid timestamp"
             else checkAuthStore reqId auth
     checkAuthStore reqId auth = do
         dupe <- isDupeAuth pool auth
         if dupe
-            then authFailure reqId "duplicate request"
+            then authFailure status403 reqId "duplicate request"
             else logAndStoreAuth reqId auth
     logAndStoreAuth reqId auth = do
         $logInfo
             ((TL.toStrict . TL.decodeUtf8) (encode (AuthSuccess reqId auth)))
         storeAuth pool auth
         liftIO (app rq respond)
-    authFailure reqId message = do
+    authFailure status reqId message = do
         let jsonMsg = encode (AuthFailure reqId message)
         $logError ((TL.toStrict . TL.decodeUtf8) jsonMsg)
         liftIO
             (respond
                  (responseLBS
-                      status401
+                      status
                       [ ( "WWW-Authenticate"
                         , "MAC error=\"" <> T.encodeUtf8 message <> "\"")]
                       jsonMsg))
